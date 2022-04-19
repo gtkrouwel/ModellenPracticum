5de132c380ad2adea8ed3735b29599196f0169d7
DavyWestra <97295197+DavyWestra@users.noreply.github.com>
2022-04-19 13:10:35 +0200

    Updated T_soil

--- DavyWestra/T_soil.py
+++ DavyWestra/T_soil.py
@@ -11,7 +11,6 @@ from pathlib import Path
 import pandas as pd
 import requests
 
-
 # Circuits coordinates
 circuit_coordinates =  {1358:(131436.240,502678.470),
                         2003:(117955.218,479433.147),
@@ -26,15 +25,13 @@ circuit_coordinates =  {1358:(131436.240,502678.470),
                         20726:(206863.49,567024.869),
                         22102:(158361.29,432857.689)}
 
-
-path_to_alliander_repo = Path(os.pardir, os.pardir, "modellenpracticum2022-speed-of-heat")
+path_to_alliander_repo = Path(".\GitHub\modellen\modellenpracticum2022-speed-of-heat")
 
 import sys
 # Make path absolute and tell Python to also look there when importing modules.
 sys.path.append(str(path_to_alliander_repo.resolve()))
 
 from weather_api import *
-
 
 DATA_PATH = "data"
 data_path = Path(DATA_PATH)
@@ -46,16 +43,21 @@ logger = logging.getLogger(__name__)
 
 downloaded_weather_cds_folder = data_path / "weather_cds_data"
 
-
 # For begin_date and end_date you can use pd.Timestamp(YYYY,MM,DD)
 def T_soil(circuitnr, begin_date: datetime.date, end_date: datetime.date,level=3):
+    #check whether the circuit number exists
+    if circuitnr not in circuit_coordinates:
+        return "circuit number doesn't exist"
+    else:
+        circuitnr_XY = circuit_coordinates.get(circuitnr)
+
     # Check for correct level input
     if level not in [1,2,3,4]:
         return "Only levels 1, 2, 3 and 4 exist"
 
     # Transforms from Dutch Coordinate System (Amersfoort / RD New) to World Coordinate System (WGS84)
     transformer = Transformer.from_crs( "EPSG:28992","EPSG:4326")
-    lat, lon = transformer.transform(circuitnr[0],circuitnr[1])
+    lat, lon = transformer.transform(circuitnr_XY[0],circuitnr_XY[1])
 
     weather_data = load_weather_data_cds(begin_date, end_date, lat, lon)
 
