9a1101205d626d4282d86ca1105815f22e69fc97
DavyWestra <97295197+DavyWestra@users.noreply.github.com>
2022-04-25 11:22:49 +0200

    Updated T_soil

--- DavyWestra/T_soil.py
+++ DavyWestra/T_soil.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from pyproj import Transformer
-
 import datetime
 import logging
 import os
@@ -25,27 +24,44 @@ circuit_coordinates =  {1358:(131436.240,502678.470),
                         20726:(206863.49,567024.869),
                         22102:(158361.29,432857.689)}
 
-path_to_alliander_repo = Path(os.pardir,os.pardir,"modellenpracticum2022-speed-of-heat")
+
+path_to_alliander_repo = Path(os.pardir, os.pardir, "modellenpracticum2022-speed-of-heat")
 
 import sys
 # Make path absolute and tell Python to also look there when importing modules.
 sys.path.append(str(path_to_alliander_repo.resolve()))
 
-from weather_api import *
-
-DATA_PATH = "data"
-data_path = Path(DATA_PATH)
-
-if not data_path.is_dir():
-    data_path.mkdir()
+from weather_api import _format_date, _create_url, _create_url_cds, _download_csv_from_url, load_weather_data
 
 logger = logging.getLogger(__name__)
 
-downloaded_weather_cds_folder = data_path / "weather_cds_data"
+downloaded_weather_cds_folder = path_to_alliander_repo / "data" / "weather_cds_data"
+
+def load_weather_data_cds(begin_date: datetime.date, end_date: datetime.date, lat, lon, circuitnr):
+    # Check if weather folder exists
+    if not downloaded_weather_cds_folder.is_dir():
+        downloaded_weather_cds_folder.mkdir()
+
+    # Format dates
+    begin_date_str = _format_date(begin_date)
+    end_date_str = _format_date(end_date)
+
+    # Create file name
+    weather_file = f"Climate_Data_Store_{begin_date_str}_{end_date_str}_{circuitnr}.csv"
+    weather_file_path = downloaded_weather_cds_folder / weather_file  # pathlib.Path object
+
+    # Check if the file exists
+    if not weather_file_path.is_file():
+        logger.info("Weather data not found in cache. Downloading new data.")
+        download_url = _create_url_cds(begin_date, end_date, lat, lon)
+        _download_csv_from_url(download_url, weather_file_path)
+
+    weather_df = pd.read_csv(weather_file_path)
+    return weather_df
 
 # For begin_date and end_date you can use pd.Timestamp(YYYY,MM,DD)
 def T_soil(circuitnr, begin_date: datetime.date, end_date: datetime.date,level=3):
-    #check whether the circuit number exists
+    #check whether the circuit number eists
     if circuitnr not in circuit_coordinates:
         return "circuit number doesn't exist"
     else:
@@ -59,7 +75,7 @@ def T_soil(circuitnr, begin_date: datetime.date, end_date: datetime.date,level=3
     transformer = Transformer.from_crs( "EPSG:28992","EPSG:4326")
     lat, lon = transformer.transform(circuitnr_XY[0],circuitnr_XY[1])
 
-    weather_data = load_weather_data_cds(begin_date, end_date, lat, lon)
+    weather_data = load_weather_data_cds(begin_date, end_date, lat, lon, circuitnr)
 
     T_soil = weather_data["soil_temperature_level_{0}".format(level)].to_numpy()
     return T_soil
